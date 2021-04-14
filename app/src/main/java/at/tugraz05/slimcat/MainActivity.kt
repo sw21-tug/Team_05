ec5765c187c8dfcd1818e40a4ac5b57d4ae78dc2
Matthias Fischer <fischer@student.tugraz.at>
2021-04-14 10:04:49 +0200

    SC-001 Clean Up and Added Buttons [PC,FM]

--- app/src/main/java/at/tugraz05/slimcat/MainActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        findViewById<FloatingActionButton>(R.id.add_cat).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
