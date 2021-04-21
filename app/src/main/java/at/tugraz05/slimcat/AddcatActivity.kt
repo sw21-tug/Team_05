6d3bd87181bb2eebad7da72c6b47634ca42c1fb0
Constantin Piber <constantin.piber@student.tugraz.at>
2021-04-21 17:52:58 +0200

    SC-010 GenderSeeker hide female-only options, color adjustments [PC,WN]

--- app/src/main/java/at/tugraz05/slimcat/AddcatActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/AddcatActivity.kt
@@ -2,9 +2,9 @@ package at.tugraz05.slimcat
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.Spinner
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class AddcatActivity : AppCompatActivity() {
@@ -18,6 +18,17 @@ class AddcatActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // make gender seeker hide female-only options
+        var genderSeeker = findViewById<SeekBar>(R.id.seek_gender)
+        var femaleSwitches = listOf(
+                findViewById<TableRow>(R.id.row_gestation),
+                findViewById<TableRow>(R.id.row_lactation),
+        )
+        genderSeeker.setOnSeekBarChangeListener(GenderSeeker(genderSeeker.progress, femaleSwitches))
+        // gender seeker helpers
+        findViewById<TextView>(R.id.label_gender_male).setOnClickListener { genderSeeker.progress = 0 }
+        findViewById<TextView>(R.id.label_gender_female).setOnClickListener { genderSeeker.progress = 1 }
+
         //Back-Button
         val actionbar = supportActionBar
         actionbar!!.title = getString(R.string.title_cat_details)
@@ -27,5 +38,27 @@ class AddcatActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+}
+
+class GenderSeeker(p: Int, private var switches: List<TableRow>) : SeekBar.OnSeekBarChangeListener {
+    init {
+        updateSwitches(p)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        Log.d("SeekBar", "%d".format(progress))
+        updateSwitches(progress)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    private fun updateSwitches(progress: Int) {
+        val visible = if (progress == 1) View.VISIBLE else View.GONE
+        switches.forEach { it.visibility = visible }
     }
 }
