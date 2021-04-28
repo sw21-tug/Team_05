cd4eba2620a0d2c6a7795cddeedcfb1c554a0a06
BensTutu <80102524+BensTutu@users.noreply.github.com>
2021-04-28 12:13:28 +0200

    ;X

--- app/src/main/java/at/tugraz05/slimcat/SettingsActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/SettingsActivity.kt
@@ -1,16 +1,51 @@
 package at.tugraz05.slimcat
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.SeekBar
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
+    companion object {
+            const val KG = 0
+            const val LF = 1
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
+        // make unit seeker
+        val unitSeeker = findViewById<SeekBar>(R.id.settings_seek_measurement)
+
+        // unit seeker helpers
+        findViewById<TextView>(R.id.settings_unit_of_measurement_kg).setOnClickListener { unitSeeker.progress = KG }
+        findViewById<TextView>(R.id.settings_unit_of_measurement_lf).setOnClickListener { unitSeeker.progress = LF
+        Toast.makeText(applicationContext,"Hallo", Toast.LENGTH_SHORT).show()}
+
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val actionBar = supportActionBar
+        actionBar!!.title = getString(R.string.title_activity_settings)
+    }
+x
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) onBackPressed()
+        return super.onOptionsItemSelected(item)
     }
 
+
+
 }
+
+
