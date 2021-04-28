6798525b9a8a6a9bb740ad2ab71b4853b829ec94
Matthias Fischer <fischer@student.tugraz.at>
2021-04-28 10:38:48 +0200

    SC-003 Switch to Settings on Click [OB.FM]

--- app/src/main/java/at/tugraz05/slimcat/MainActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/MainActivity.kt
@@ -38,9 +38,11 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val id = item.itemId
+        if (id == R.id.action_settings) {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/at/tugraz05/slimcat/SettingsActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/SettingsActivity.kt
@@ -9,18 +9,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.settings, SettingsFragment())
-                    .commit()
-        }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
-    }
 }
