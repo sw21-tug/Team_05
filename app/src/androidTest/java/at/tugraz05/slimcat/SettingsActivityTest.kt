a157fb684bc526edee56a07b54555e14c61f267e
BensTutu <80102524+BensTutu@users.noreply.github.com>
2021-05-04 23:11:04 +0200

    SC-003 Load data test[OB,FM]

--- app/src/androidTest/java/at/tugraz05/slimcat/SettingsActivityTest.kt
+++ app/src/androidTest/java/at/tugraz05/slimcat/SettingsActivityTest.kt
@@ -1,5 +1,8 @@
 package at.tugraz05.slimcat
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
@@ -10,8 +13,9 @@ import org.junit.runner.RunWith
 import android.widget.SeekBar
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.*
+import junit.framework.Assert.assertEquals
 import org.hamcrest.CoreMatchers
-
+import org.mockito.Mockito
 
 
 @RunWith(AndroidJUnit4::class)
@@ -40,4 +44,28 @@ class SettingsActivityTest {
         onView(withId(R.id.settings_unit_of_measurement_lf)).perform(click())
         onView(withId(R.id.settings_unit_of_measurement)).check(assertView<SeekBar> { assertThat(it.progress, CoreMatchers.equalTo(SettingsActivity.LF)) })
     }
+
+    @Test
+    fun testLoadData() {
+        val scenario = ActivityScenario.launch(SettingsActivity::class.java)
+        val sharedPreferences = Mockito.mock(SharedPreferences::class.java)
+        val context = Mockito.mock(Context::class.java)
+        Mockito.`when`(context.getSharedPreferences("userprefs", MODE_PRIVATE)).thenReturn(sharedPreferences)
+        Mockito.`when`(sharedPreferences.getString("name","" )).thenReturn("test")
+        Mockito.`when`(sharedPreferences.getString("email","" )).thenReturn("test@email")
+        Mockito.`when`(sharedPreferences.getInt("gender", 0)).thenReturn(0)
+        Mockito.`when`(sharedPreferences.getInt("language", 0)).thenReturn(0)
+        Mockito.`when`(sharedPreferences.getInt("unit", 0)).thenReturn(0)
+        Mockito.`when`(sharedPreferences.getString("image","" )).thenReturn("testimage")
+        scenario.onActivity {
+            val user = it.loadData(context)
+            assertEquals(user.name, "test")
+            assertEquals(user.email, "test@email")
+            assertEquals(user.gender, 0)
+            assertEquals(user.language, 0)
+            assertEquals(user.unit, 0)
+            assertEquals(user.image, "testimage")
+        }
+    }
+
 }
