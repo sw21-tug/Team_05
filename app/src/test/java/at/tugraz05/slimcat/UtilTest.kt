50826509caf2060da9abbe5cd7daa845caba2f0c
Bibo <baher.el-banna@student.tugraz.at>
2021-05-06 15:07:40 +0200

    SC-024 implemented day test with same month [EB, LC]

--- app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
+++ app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
@@ -23,5 +23,25 @@ class UtilTest{
 
         assert(age == 4)
     }
+
+    @Test
+    fun testCalcAgeDayBefore(){
+        val date_of_birth = LocalDate.of(2016, 5, 1)
+        val current_date = LocalDate.of(2021, 5, 6)
+
+        val age = Util.calculateAge(date_of_birth, current_date)
+
+        assert(age == 5)
+    }
+
+    @Test
+    fun testCalcAgeDayAfter(){
+        val date_of_birth = LocalDate.of(2016, 5, 1)
+        val current_date = LocalDate.of(2021, 5, 6)
+
+        val age = Util.calculateAge(date_of_birth, current_date)
+
+        assert(age == 4)
+    }
 }
 
