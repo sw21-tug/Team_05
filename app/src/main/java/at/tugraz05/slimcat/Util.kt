ee4f294ec6da3d37eb2f9add608823c6ad63b260
Christoph Ladler <christoph.ladler@student.tugraz.at>
2021-05-06 15:00:10 +0200

    SC-024 implemented age calculation [EB, LC]

--- app/src/main/java/at/tugraz05/slimcat/Util.kt
+++ app/src/main/java/at/tugraz05/slimcat/Util.kt
@@ -1,11 +1,13 @@
 package at.tugraz05.slimcat
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalDate
 
-
 object Util {
+    @RequiresApi(Build.VERSION_CODES.O)
     fun calculateAge(date_of_birth: LocalDate, current_date : LocalDate) : Int{
-        var age = 0
+        var age = current_date.year - date_of_birth.year
         return age
     }
 }
