037ab0140132246cd17dfb32b2dfb91521695d49
Christoph Ladler <christoph.ladler@student.tugraz.at>
2021-05-06 15:01:55 +0200

    SC-024 added test case for age calculation [EB,LC]

--- app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
+++ app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
@@ -13,5 +13,15 @@ class UtilTest{
 
         assert(age == 5)
     }
+
+    @Test
+    fun testCalcAgeMonthAfter(){
+        val date_of_birth = LocalDate.of(2016, 12, 1)
+        val current_date = LocalDate.of(2021, 5, 6)
+
+        val age = Util.calculateAge(date_of_birth, current_date)
+
+        assert(age == 4)
+    }
 }
 
