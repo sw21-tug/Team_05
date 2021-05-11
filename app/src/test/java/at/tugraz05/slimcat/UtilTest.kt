12477a9da3f8fdc3c36b25a4601aab61fb0b28db
Piki2000 <patrick.andrawes20@gmail.com>
2021-05-11 12:38:38 +0200

    SC-004 add Test for calorie recommendation [OB, AP]

--- app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
+++ app/src/test/java/at/tugraz05/slimcat/UtilTest.kt
@@ -3,9 +3,9 @@ package at.tugraz05.slimcat
 import org.junit.Test
 import java.time.LocalDate
 
-class UtilTest{
+class UtilTest {
     @Test
-    fun testCalcAgeMonthBefore(){
+    fun testCalcAgeMonthBefore() {
         val date_of_birth = LocalDate.of(2016, 1, 1)
         val current_date = LocalDate.of(2021, 5, 6)
 
@@ -15,7 +15,7 @@ class UtilTest{
     }
 
     @Test
-    fun testCalcAgeMonthAfter(){
+    fun testCalcAgeMonthAfter() {
         val date_of_birth = LocalDate.of(2016, 12, 1)
         val current_date = LocalDate.of(2021, 5, 6)
 
@@ -25,7 +25,7 @@ class UtilTest{
     }
 
     @Test
-    fun testCalcAgeDayBefore(){
+    fun testCalcAgeDayBefore() {
         val date_of_birth = LocalDate.of(2016, 5, 1)
         val current_date = LocalDate.of(2021, 5, 6)
 
@@ -35,13 +35,29 @@ class UtilTest{
     }
 
     @Test
-    fun testCalcAgeDayAfter(){
+    fun testCalcAgeDayAfter() {
         val date_of_birth = LocalDate.of(2016, 5, 12)
         val current_date = LocalDate.of(2021, 5, 6)
 
         val age = Util.calculateAge(date_of_birth, current_date)
 
         assert(age == 4)
+    }
+
+    @Test
+    fun testCalorieRecommendation() {
+        val weight: Double = 6.0
+        val obese = true
+        val overweightProne = false
+        val hospitalized = true
+        val neutered = false
+        val gestation = false
+        val lactation = false
+
+        val calRec = Util.calculateCalories(weight, obese, overweightProne, hospitalized,
+            neutered, gestation, lactation)
+
+        assert(calRec == 510)
     }
 }
 
