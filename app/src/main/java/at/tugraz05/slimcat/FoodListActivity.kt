9fbfff7c12cd35f625a6d02040800295af3c99a6
Matthias Fischer <fischer@student.tugraz.at>
2021-05-19 10:29:30 +0200

    SC-031 Created new FoodList Activity [FM,VT]

---
+++ app/src/main/java/at/tugraz05/slimcat/FoodListActivity.kt
@@ -0,0 +1,11 @@
+package at.tugraz05.slimcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_list)
+    }
+}
