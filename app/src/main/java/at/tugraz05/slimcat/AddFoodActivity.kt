81f3534ae522c04f685d3056cf2586f145c80ad5
Toml499 <thomas.vegh@yahoo.de>
2021-05-25 17:06:42 +0200

    SC-031 new AddFoodActivity and FoodDetails Data Class [FM, VT]

---
+++ app/src/main/java/at/tugraz05/slimcat/AddFoodActivity.kt
@@ -0,0 +1,11 @@
+package at.tugraz05.slimcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
