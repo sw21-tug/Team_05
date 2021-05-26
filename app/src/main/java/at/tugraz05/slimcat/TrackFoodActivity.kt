6f5976c7cc067215b74190eb1635ebc6fa15e9f0
Piki2000 <patrick.andrawes20@gmail.com>
2021-05-26 11:52:07 +0200

    SC-014 add track food element [PC, AP]

--- app/src/main/java/at/tugraz05/slimcat/TrackFoodActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/TrackFoodActivity.kt
@@ -1,6 +1,7 @@
 package at.tugraz05.slimcat
 
 import android.os.Bundle
+import android.widget.TableLayout
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -9,6 +10,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import at.tugraz05.slimcat.databinding.ActivityTrackFoodBinding
+import at.tugraz05.slimcat.databinding.ActivityTrackFoodElementBinding
+import at.tugraz05.slimcat.databinding.CatAccordionBinding
 
 class TrackFoodActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrackFoodBinding
@@ -17,6 +20,16 @@ class TrackFoodActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_track_food)
+
+        val container = findViewById<TableLayout>(R.id.scroll_track_food)
+        val cat:CatDataClass = intent.extras!!.getParcelable("Cat")!!
+
+        Food.foods.forEach{
+            val binding = DataBindingUtil.inflate<ActivityTrackFoodElementBinding>(layoutInflater, R.layout.activity_track_food_element, container, false)
+            binding.cat = cat
+            binding.food = it
+            container.addView(binding.root)
+        }
 
         val actionbar = supportActionBar
         actionbar!!.title = getString(R.string.catlist_track_food)
