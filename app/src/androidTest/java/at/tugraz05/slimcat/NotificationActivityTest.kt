0106be390667d279e859930f2f01b32f34fe0fe4
BensTutu <benedict.osei-tutu@student.tugraz.at>
2021-06-10 01:13:23 +0200

    SC-012 add action button tests [OB, AP]

--- app/src/androidTest/java/at/tugraz05/slimcat/NotificationActivityTest.kt
+++ app/src/androidTest/java/at/tugraz05/slimcat/NotificationActivityTest.kt
@@ -1,12 +1,15 @@
 package at.tugraz05.slimcat
 
+import androidx.preference.PreferenceManager
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.TestCase
+import org.hamcrest.core.IsNot
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -50,5 +53,45 @@ class NotificationActivityTest : TestCase() {
         Espresso.onView(ViewMatchers.withId(R.id.chk_Saturday))
         Espresso.onView(ViewMatchers.withId(R.id.chk_Sunday))
             .perform(ViewActions.scrollTo()).perform(ViewActions.click())
+    }
+
+    @Test
+    fun notificationButtonIsDisplayed() {
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(targetContext).edit()
+        preferencesEditor.clear()
+        ActivityScenario.launch(NotificationsActivity::class.java)
+        Espresso.onView(ViewMatchers.withId(R.id.action_button_notification))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun notificationButtonTest() {
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(targetContext).edit()
+        preferencesEditor.clear()
+        ActivityScenario.launch(NotificationsActivity::class.java)
+        Espresso.onView(ViewMatchers.withId(R.id.action_button_notification))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.txt_timePicker))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.txt_notifications))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_timePicker))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Monday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Tuesday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Wednesday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Thursday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Friday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Saturday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.chk_Sunday))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
     }
 }
