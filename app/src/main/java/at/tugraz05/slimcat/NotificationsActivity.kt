63f5651f293e239eb124bf10bbe34ef6f3bafaf6
Piki2000 <patrick.andrawes20@gmail.com>
2021-06-10 00:17:22 +0200

    SC-012 add menu notifications and checkboxes [OB, AP]

--- app/src/main/java/at/tugraz05/slimcat/NotificationsActivity.kt
+++ app/src/main/java/at/tugraz05/slimcat/NotificationsActivity.kt
@@ -2,10 +2,26 @@ package at.tugraz05.slimcat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class NotificationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notifications)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val actionBar = supportActionBar
+        actionBar!!.title = getString(R.string.title_activity_notifications)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_notifications, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
