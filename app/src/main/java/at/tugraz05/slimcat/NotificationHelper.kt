a31e25a94dbc5a73a871588aa738e9d1a14a51b3
Piki2000 <patrick.andrawes20@gmail.com>
2021-06-10 02:32:24 +0200

    SC-012 add Notification Helper class [OB, AP]

---
+++ app/src/main/java/at/tugraz05/slimcat/NotificationHelper.kt
@@ -0,0 +1,49 @@
+package at.tugraz05.slimcat
+
+import android.annotation.TargetApi
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.ContextWrapper
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class NotificationHelper(base: Context?) : ContextWrapper(base) {
+    companion object {
+        const val channelID = "channelID"
+        const val channelName = "Channel"
+        const val VISIBILITY_PRIVATE = 0
+    }
+
+    private var nManager: NotificationManager? = null
+    @TargetApi(Build.VERSION_CODES.O)
+    fun createChannel() {
+        val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        channel.lightColor = R.color.design_default_color_primary
+        channel.lockscreenVisibility = VISIBILITY_PRIVATE
+        manager!!.createNotificationChannel(channel)
+    }
+
+    val manager: NotificationManager?
+        get() {
+            if (nManager == null) {
+                nManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            }
+            return nManager
+        }
+
+    fun getChannelNotification(title: String?, message: String?): NotificationCompat.Builder {
+        return NotificationCompat.Builder(applicationContext, channelID)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+    }
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel()
+        }
+    }
+}
